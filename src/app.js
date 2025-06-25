0429845b2c3b2395db238613ee4b80f7acf70611
SleepyDut <diegoundreiner@gmail.com>
2025-06-25 20:50:04 +0000

    finished project, card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import "bootstrap";
 import "./style.css";
 
+let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+let symbols = ["♦", "♥", "♠", "♣"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function () {
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Genera un numero/letra random
+  let randNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  const generadosNumber = document.getElementsByClassName('centro');
+
+  for (let i = 0; i < generadosNumber.length; i++) {
+    generadosNumber[i].innerText = randNumber;
+  }
+
+  // Genera un palo random
+  let randSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+  const generadosPalos = document.getElementsByClassName('palos');
+
+  for (let i = 0; i < generadosPalos.length; i++) {
+    generadosPalos[i].innerText = randSymbol;
+    // establece un condicional para ponerlo de un color
+    if (generadosPalos[i].innerText === "♦" || generadosPalos[i].innerText === "♥") {
+      generadosPalos[i].style.color = "red";
+    }
+  }
 };
+
+
